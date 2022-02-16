4828cabc02820bac9b16f7689fa8e9ceea433a45
Marian-Janolek <m.janolek@gmail.com>
2022-02-16 19:56:57 +0100

    add register, login and localstorage

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const colors = require('colors');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 const connectDB = require('./db/db');
+const morgan = require('morgan');
 
 connectDB();
 
@@ -11,6 +12,7 @@ const userRouter = require('./routes/userRoutes');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
